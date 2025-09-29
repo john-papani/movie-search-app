89c37a0849b765a9a20ff1caa9006b3c24ebaa0e
john-papani <johnpapani1@gmail.com>
2025-09-29 15:42:35 +0300

    Fix duplicate movies result

--- src/app/Components/Movies.js
+++ src/app/Components/Movies.js
@@ -3,11 +3,14 @@ import Moviecard from "./Moviecard";
 import ErrorCard from "./ErrorCard";
 
 function Movies({ moviesData }) {
+  const uniqueMovies = moviesData?.Search
+    ? Array.from(new Map(moviesData.Search.map((m) => [m.imdbID, m])).values())
+    : [];
+
   return (
     <div className="flex flex-row flex-wrap justify-center w-full md:w-[85vw] h-fit mx-auto md:h-[80vh] overflow-y-auto mt-4 md:gap-4">
-      {moviesData &&
-        moviesData.Search &&
-        moviesData.Search.map((movie) => (
+      {uniqueMovies.length > 0 &&
+        uniqueMovies.map((movie) => (
           <Moviecard key={movie.imdbID} movieData={movie} />
         ))}
       {moviesData && moviesData.Response === "False" && (
